82c5b9d1876e9a3dacd5112c4b90d0b523cea245
ash12-coder <68694966+ash12-coder@users.noreply.github.com>
2020-10-17 11:44:26 +0530

    Add files via upload

---
+++ left.js
@@ -0,0 +1,10 @@
+function show(){
+    document.getElementById("main").style.transform = "translateX(0px)";
+    document.getElementById("main").style.transition = "2s";
+}
+function hide(){
+    document.getElementById("main").style.transform = "translateX(-250px)";
+}
+function hides(){
+    document.getElementById("main").style.transform = "translateX(-250px)";
+}
